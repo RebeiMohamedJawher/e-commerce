010d1234dfc646e65b00c41aa91bfb56ba7cb79b
RebeiMohamedJawher <mohamedjawher.rebei@supcom.tn>
2020-11-23 23:17:03 +0100

    ....

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  
+    apiKey: "AIzaSyAL0lEUxsAuhl0l4_fuP5wSMu8KKvmjQiQ",
+    authDomain: "e-commerce-ecbf8.firebaseapp.com",
+    databaseURL: "https://e-commerce-ecbf8.firebaseio.com",
+    projectId: "e-commerce-ecbf8",
+    storageBucket: "e-commerce-ecbf8.appspot.com",
+    messagingSenderId: "22351378928",
+    appId: "1:22351378928:web:6004d77125eabeb7934535",
+    measurementId: "G-Y4S7SRYF1D"
+  });
+
+const auth = firebase.auth();
+
+export { auth };
+
