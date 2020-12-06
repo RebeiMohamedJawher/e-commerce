06490c76c45b23397be7a90c27c42c8d66756e47
RebeiMohamedJawher <mohamedjawher.rebei@supcom.tn>
2020-12-06 22:34:31 +0100

    ....

--- src/components/Checkout/ShoeItem/ShoeItem.js
+++ src/components/Checkout/ShoeItem/ShoeItem.js
@@ -21,11 +21,8 @@ const ShoeItem = ({ id, image, title, before, after, category }) => {
       <div className="shoeItem__right">
         <span className="shoeItem__title">{title}</span>
         <span className="shoeItem__category">{category}</span>
-        {/* <div className="shoeItem__price">
-          <span className="shoeItem__before">Before: <span className="shoeItem__beforePrice">{before}</span></span>
-          <span className="shoeItem__after">After: <span className="shoeItem__afterPrice">{after}</span></span>
-        </div> */}
-        <span className="shoeItem__price">Price:  <span className="shoeItem__afterPrice"> ${after}</span></span>
+        {}
+        <span className="shoeItem__price">Price:  <span className="shoeItem__afterPrice"> {after}DNT</span></span>
         <div className="shoeItem__remove" onClick={removeFromCart} >
           <span className="shoeItem__click">Remove From Cart</span>
           <ShoppingCartRoundedIcon />
