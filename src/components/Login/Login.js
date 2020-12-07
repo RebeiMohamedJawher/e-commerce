d2f4bc7a6b46644b8fe9276354cba210acf48334
RebeiMohamedJawher <mohamedjawher.rebei@supcom.tn>
2020-12-07 19:20:23 +0100

    .....

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -33,9 +33,7 @@ const Login = () => {
       .
       <div className="login__page">
         <div className="logo">
-          <img className="login__logo" src="https://seeklogo.net/wp-content/uploads/2013/11/nike-swoosh-vector-logo-400x400.png"  alt="Nike logo" />
         </div>
-        <span className="login__head1">Get News and Offers from Nike</span>
         <form>
           <span className="login__email">Email Address:</span>
           <input 
@@ -56,9 +54,7 @@ const Login = () => {
             required 
           />
           <button className="login__button" type="submit" onClick={loginAccount} >Login</button>
-          <span className="login__head2">Sign Up for emails to get special news and offers from the Nike family of brands</span>
           <button className="login__button" type="submit" onClick={createAccount} >Create your Account</button>
-          <span className="login__head3">By signing up, you agree to Nike's <span className="underlineHead3">Privacy Policy</span> and <span className="underlineHead3">Terms of Use</span></span>
         </form>
       </div>
       .
